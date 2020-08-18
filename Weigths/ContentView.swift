14a5472a91256fd12fd7a73cc9185c6438ed55d4
Sergio Baró <serbarsim@gmail.com>
2020-08-18 19:39:01 +0200

    Initial Commit

---
+++ Weigths/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Weigths
+//
+//  Created by Sergio Baro on 18/08/2020.
+//  Copyright © 2020 Sergio Baro. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
